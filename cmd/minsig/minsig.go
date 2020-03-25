0e4417ff0856914a0929b3fed894bf6e371e94f9
salman <s@aljmz.com>
2020-03-25 13:14:49 -0400

    minsig: general cleanup
    Replace condition variable with channel and tidy the html index page.

--- cmd/minsig/minsig.go
+++ cmd/minsig/minsig.go
@@ -1,6 +1,6 @@
 // Server minsig is a WebRTC signalling server.
 //
-// It facilitates establishing WebRTC connections between peer using ephemeral
+// It facilitates establishing WebRTC connections between peers using ephemeral
 // slots to hold WebRTC offers and answers.
 package main
 
@@ -19,15 +19,14 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
-type sessiondesc struct {
+type sdp struct {
 	Type string `json:"type"`
 	SDP  string `json:"sdp"`
 }
 
 type slot struct {
-	offer  *sessiondesc
-	answer *sessiondesc
-	c      *sync.Cond
+	offer  sdp
+	answer chan sdp
 }
 
 var slots = struct {
@@ -37,83 +36,58 @@ var slots = struct {
 
 func serveHTTP(w http.ResponseWriter, r *http.Request) {
 	slotkey := r.URL.Path
-
 	if r.Method == http.MethodGet && slotkey == "/" {
 		w.Write([]byte(indexpage))
 		return
 	}
-
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	if r.Method != http.MethodPost {
 		http.Error(w, "invalid method", 400)
 	}
 
-	enc := json.NewEncoder(w)
-	dec := json.NewDecoder(r.Body)
-	var msg sessiondesc
-	err := dec.Decode(&msg)
+	var msg sdp
+	err := json.NewDecoder(r.Body).Decode(&msg)
 	if err != nil {
-		log.Printf("%v", err)
+		http.Error(w, "could not decode body", 400)
 		return
 	}
 
-	log.Printf("%v: post", slotkey)
+	log.Printf("%v: %v", slotkey, msg.Type)
 
 	slots.Lock()
-	slot := slots.m[slotkey]
-	if slot == nil {
-		if msg.Type != "offer" {
-			slots.Unlock()
-			log.Printf("%v: [1] not an offer", slotkey)
-			http.Error(w, "invalid offer description", 400)
-			return
-		}
-
-		// New offer (probably)
-		s = &slot{
-			offer: &msg,
-			c:     sync.NewCond(&sync.Mutex{}),
-		}
-		s.c.L.Lock()
-		slots.m[slotkey] = slot
+	s := slots.m[slotkey]
+	switch {
+	case s != nil && msg.Type == "offer":
+		// Already have offer, pass that down
 		slots.Unlock()
-
-		answered := make(chan struct{})
-		go func() {
-			for s.answer == nil {
-				s.c.Wait()
-			}
-			err := enc.Encode(slot.answer)
-			s.c.L.Unlock()
+		err := json.NewEncoder(w).Encode(s.offer)
+		if err != nil {
+			log.Printf("%v", err)
+		}
+	case s != nil && msg.Type == "answer":
+		// This is an answer to an offer, wake the other go routines up.
+		slots.Unlock()
+		s.answer <- msg
+	case s == nil && msg.Type == "offer":
+		// This is a new offer.
+		s = &slot{offer: msg, answer: make(chan sdp)}
+		slots.m[slotkey] = s
+		slots.Unlock()
+		select {
+		case a := <-s.answer:
+			err := json.NewEncoder(w).Encode(a)
 			if err != nil {
 				log.Printf("%v", err)
 			}
-			answered <- struct{}{}
-		}()
-
-		// TODO refactor this mess. (ctx is done when this func returns)
-		select {
-		case <-answered:
 		case <-r.Context().Done():
 		}
-
 		slots.Lock()
 		delete(slots.m, slotkey)
 		slots.Unlock()
-	} else {
+	default:
+		// Any other state is invalid.
 		slots.Unlock()
-		if msg.Type == "offer" {
-			// Already have offer, pass that down
-			err := enc.Encode(slot.offer)
-			if err != nil {
-				log.Printf("%v", err)
-				return
-			}
-		} else if msg.Type == "answer" {
-			// This is an answer to an offer, wake the other go routines up.
-			slot.answer = &msg
-			slot.c.Broadcast()
-		}
+		http.Error(w, "invalid offer description", 400)
 	}
 }
 
@@ -139,7 +113,11 @@ func main() {
 		WriteTimeout: 120 * time.Second,
 		IdleTimeout:  20 * time.Second,
 		Addr:         *httpaddr,
-		Handler:      m.HTTPHandler(http.HandlerFunc(serveHTTP)),
+		Handler:      m.HTTPHandler(nil),
+	}
+	// Enable non-redirect plaintext http handler if https is disabled.
+	if *httpsaddr == "" {
+		srv.Handler = m.HTTPHandler(http.HandlerFunc(serveHTTP))
 	}
 	go func() { log.Fatal(srv.ListenAndServe()) }()
 
@@ -155,102 +133,47 @@ func main() {
 }
 
 var indexpage = `
-<!DOCTYPE html>
+<!doctype html>
 <meta charset=utf-8>
-<link rel="canonical" href="https://minimimsignal.0f.io/">
 <title>minimum signal</title>
 <style>
-body {
-  font: small arial, sans-serif;
-  max-width: 32em;
-  margin: auto;
-  padding: 2em;
-  background: #fff;
-  color: #000;
-}
-pre {
-  font: small Inconsolata, monospace;
-  word-spacing: 0;
-  letter-spacing: 0;
-}
-a {
-  text-decoration: none;
-}
-h1, h2, h3, h4, caption, thead th {
-  font-weight: normal;
-  font-variant: small-caps;
-  text-shadow: 0 0 1px #667;
-}
-h1 {
-  font-size: 1.7em;
-  text-align: center;
-  width: 100%;
-}
-h2 {
-  font-size:1.6em;
-}
-h3 {
-  font-size: 1.1em;
-}
-footer {
-  font-size: x-small;
-  text-align: center;
-}
+body {font: small arial, sans-serif;max-width: 40em;margin: auto;padding: 2em;}
+pre {font: small Inconsolata, monospace;word-spacing: 0;letter-spacing: 0;}
+h1 {font-size: 1.7em;text-align: center;}
+h2 {font-size:1.6em;}
+h3 {font-size: 1.1em;}
+footer {font-size: x-small;text-align: center;}
 </style>
-<body>
 
 <h1>MINIMUM SIGNAL</h1>
-
 <p>Experimental service to handle <a href="https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API">WebRTC</a> singalling so you don't have to.</p>
 
 <h2>RATIONALE</h2>
-
 <p>While WebRTC's main selling point is that it is peer-to-peer, every WebRTC application needs a central signalling server to facilitate establishing this direct connection.</p>
-
 <p>Writing these is somewhat tedious and requires setting up the infrastructure to host it. What if this existed as a service that you could just use and focus on building the client side of your WebRTC application?</p>
-
 <p>This way, no special server-side code needs to be written. The client parts (HTML/JS/CSS) could be hosted on some static service like S3 or GitHub Pages, or they could be native applications.</p>
 
 <h2>MODEL</h2>
-
-<p>WebRTC uses an "offer" and "answer" model, where one party puts sends an "offer" encoded in a JSON object and the other party responds similarly with an "answer" JSON object. Minimum signal uses a slot system to allow clients to exchange offers and answers.</p>
-
-<p>Slots are arbitrary strings, currently capped at 255 bytes. If Alice wants to reach Bob, then they or their user agents perform the following steps:</p>
-
+<p>WebRTC uses an "offer" and "answer" model, where one party puts sends an "offer" encoded in a JSON object and the other party responds similarly with an "answer" JSON object. Minimum Signal uses a slot system to allow clients to exchange offers and answers. Slots are arbitrary strings, currently capped at 125 bytes. They can be communicated out of band. E.g. SMS, AirDrop, email, or shouted out across the room.</p>
+<p>If two peers want to establish a connection:</p>
 <ol>
-<li>A uploads its offer object to Minimum Signal at some arbitrary slot.</li>
-<li>A communicates the slot name to B out of band. E.g. message, AirDrop, email, or shout it out across the room.</li>
-<li>B fetches A's offer and uploads its own.
-<li>A receives B's offer and they both carry on the WebRTC nogotiations directly.
+<li>Both attempt to post their offers to the same slot.</li>
+<li>Whichever gets their offer up first (A) waits for a response. The other peer (B) will get A's offer in the response when they attempt to upload theirs.</li>
+<li>B discards their original offer, generates an answer based on A's offer, and posts it to the same slot.</li>
+<li>A will receive B's answer and they both carry on the WebRTC nogotiations directly.</li>
+<li>At this point, Minimum Signal's role is finished and the slot is free to be used by someone else.</li>
 </ol>
-
-<p>At this point, Minimum Signal's role is finished and the slot is free to be used by someone else. This slot model is similar to what the non-crypto parts of <a href="https://github.com/warner/magic-wormhole">Magic Wormhole</a> use.</p>
+<p>This slot model is similar to what the non-crypto parts of <a href="https://github.com/warner/magic-wormhole">Magic Wormhole</a> use.</p>
 
 <h2>API</h2>
-
-<p>There is only one endpoint supported:</p>
-<pre>https://minimumsignal.0f.io/$slot</pre>
-<p>where $slot is the slot name.</p>
-<p>There is only one method supported, POST with the SDP as body.</p>
-<p>If the SDP is of type "offer" and the slot is free, the request will block until someone uploads an answer to the same slot, at which point it will return the answer.
-<p>If the SDP is of type "offer" and the slot is busy, the response will be the original offer.
-<p>If the SDP is of type "answer", it will be forwarded to the original sender of the offer (who up until this point has been blocked).
+<pre>POST https://example.com/:slot</pre>
+<p>If the body is an offer and the slot is free, the request will block until someone uploads an answer to the same slot, at which point it will return the answer.
+<p>If the body is an offer and the slot is busy, the response will be the original offer.
+<p>If the body is an answer, it will be forwarded to the original sender of the offer.
 <p>All other requests are invalid.</p>
 
-<p>The intended usage is that both parties, A and B, race to upload their offers to the same slot. Whichever of them loses has to accept the other one's offer and upload an answer based on it.
-
-<h2>SECURITY CONSIDIRATIONS</h2>
-
-<p>On its own, this scheme is not secure.</p>
-
-<p>In the best case, assuming the slot name is a long and difficult to guess string, the trust model would still have to include the operator of the signalling server, since they can see and potentially modify both parties' SDPs.</p>
-
-<p>For a demo that might be good enough, but for any useful application you'll need to implement a way for A to authenticate B on this potentially untrusted link. Some PAKE might be a good way to do it and fits well with the slot system. Again, cf. Magic Wormhole.</p>
-
 <h2>USAGE EXAMPLE</h2>
-
 <p>Here's some example JavaScript to demostrate the usage of the API. The dial() function returns an RTCPeerConnection object.</p>
-
 <pre>
 let dial = async (slot, config) => {
 	let initconn = pc => {
@@ -268,7 +191,7 @@ let dial = async (slot, config) => {
 	// Wait for ICE candidates.
 	await new Promise(r=>{pc.onicecandidate=e=>{if(e.candidate === null){r()}}})
 	// Upload offer.
-	let response = await fetch("https://minimumsignal.0f.io/"+slot, {
+	let response = await fetch("https://example.com/"+slot, {
 		method: 'POST',
 		body: JSON.stringify(pc.localDescription)
 	})
@@ -286,7 +209,7 @@ let dial = async (slot, config) => {
 		await new Promise(r=>{pc.onicecandidate=e=>{if(e.candidate === null){r()}}})
 
 		// Upload answer.
-		await fetch("https://minimumsignal.0f.io/"+slot, {
+		await fetch("https://example.com/"+slot, {
 			method: 'POST',
 			body: JSON.stringify(pc.localDescription)
 		})
@@ -298,15 +221,17 @@ let dial = async (slot, config) => {
 }
 </pre>
 
-<h2>LIMITATIONS</h2>
+<h2>SECURITY CONSIDIRATIONS</h2>
+<p>On its own, this scheme is not secure.</p>
+<p>In the best case, assuming the slot name is a long and difficult to guess string, the trust model would still have to include the operator of the signalling server, since they can see and potentially modify both parties' SDPs.</p>
+<p>For a demo that might be good enough, but for any useful application you'll need to implement a way for A to authenticate B on this potentially untrusted link. Some PAKE might be a good way to do it and fits well with the slot system. Again, cf. Magic Wormhole.</p>
 
-There is no support for <a href="https://tools.ietf.org/html/draft-ietf-ice-trickle-21">Trickle ICE</a>. The offer and answer must have all candidates to be considered.
+<h2>LIMITATIONS</h2>
+<p>There is no support for <a href="https://tools.ietf.org/html/draft-ietf-ice-trickle-21">Trickle ICE</a>. The offer and answer must have all candidates to be considered.</p>
 
 <h2>DISCLAIMER</h2>
-
-<p>The authors takes absolutely no responsibity and offers no promises for the reliability or availability of this experiment.</p>
-
-<p>We reserve the right to call it quits any time. If Google can do this we sure can.</p>
+<p>The authors offer an instance of this service hosted at https://minimumsignal.0f.io/. The authors takes absolutely no responsibity and offers no promises for the reliability or availability of this experiment.</p>
+<p>We reserve the right to call quits any time. If Google can do this we sure can.</p>
 
 <footer>
 Comments &amp; complaints <a href="https://0x65.net" rel="author">salman aljammaz</a>: <a href="https://twitter.com/_saljam">@_saljam</a> or <a href="mailto:s@aljmz.com">s@aljmz.com</a>
