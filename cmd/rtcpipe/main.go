db77c9a80f58494d5383d34c92b43c24b47ec9aa
salman <s@aljmz.com>
2020-03-26 18:49:33 -0400

    rtcpipe: use spake2 tunnel

--- cmd/rtcpipe/main.go
+++ cmd/rtcpipe/main.go
@@ -15,11 +15,14 @@ func main() {
 	iceserv := flag.String("ice", "stun:stun.l.google.com:19302", "stun or turn servers to use")
 	sigserv := flag.String("minsig", "https://minimumsignal.0f.io/", "signalling server to use")
 	flag.Parse()
-	if flag.NArg() != 1 {
+	if flag.NArg() != 2 {
 		flag.PrintDefaults()
 		os.Exit(-1)
 	}
+	// TODO use similar dictionaries and code format to magic wormhole?
+	// TODO generate and print slots and passwords
 	slot := flag.Arg(0)
+	pass := flag.Arg(1)
 
 	rtccfg := webrtc.Configuration{}
 	if *iceserv != "" {
@@ -35,27 +38,31 @@ func main() {
 		log.Fatalf("could not dial: %v", err)
 	}
 
+	// TODO use signalling server generated nonse in identity
+	t, err := NewTunnel(pass, slot, c)
+	if err != nil {
+		log.Fatalf("could establish tunnel: %v", err)
+	}
+
 	done := make(chan struct{})
 	// The recieve end of the pipe.
 	go func() {
-		_, err := io.Copy(os.Stdout, c)
+		_, err := io.Copy(os.Stdout, t)
 		if err != nil {
 			log.Printf("could not write to stdout: %v", err)
 		}
 		//log.Printf("debug: rx %v", n)
 		done <- struct{}{}
 	}()
-
 	// The send end of the pipe.
 	go func() {
-		_, err := io.Copy(c, os.Stdin)
+		_, err := io.Copy(t, os.Stdin)
 		if err != nil {
 			log.Printf("could not write to channel: %v", err)
 		}
 		//log.Printf("debug: tx %v", n)
 		done <- struct{}{}
 	}()
-
 	<-done
 	c.Close()
 }
