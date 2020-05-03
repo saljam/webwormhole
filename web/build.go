f8890377de9a457aeefdcb7ac9a170a242e3862a
dnc <git@d10.dev>
2020-05-03 10:45:27 -0700

    use build tags and package names for cleaner go generate build

--- web/build.go
+++ web/build.go
@@ -1,4 +1,6 @@
-package main
+// +build !js,!wasm
+
+package webwormholeutil // use `go generate` to build wasm
 
 //go:generate sh -c "GOOS=js GOARCH=wasm go build -o util.wasm "
 //go:generate sh -c "cp $(go env GOROOT)/misc/wasm/wasm_exec.js ."
