be6a18862a3d4a55419cba84a7e56f4f28f006ec
salman <s@aljmz.com>
2021-03-28 19:10:21 +0300

    set TLS minimum version to 1.2
    It was optimistically set to 1.3, but iOS Safari seems to only do 1.2 (!)
    for WebSockets. Drop it to 1.2 until we figure this one out.
    
    The CipherSuites used are ones recommeneded in:
        https://ssl-config.mozilla.org/#server=go&config=intermediate

--- cmd/ww/server.go
+++ cmd/ww/server.go
@@ -357,7 +357,17 @@ func server(args ...string) {
 		IdleTimeout:  20 * time.Second,
 		Addr:         *httpsaddr,
 		Handler:      http.HandlerFunc(handler),
-		TLSConfig:    &tls.Config{MinVersion: tls.VersionTLS13},
+		TLSConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			},
+		},
 	}
 	srv := &http.Server{
 		ReadTimeout:  10 * time.Second,
