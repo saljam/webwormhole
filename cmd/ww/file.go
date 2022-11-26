5bd9013c920ef94363e3b78c520861b0b15edad5
Ali Mosajjal <hi@n0p.me>
2022-11-26 19:02:10 +1300

    potentially a better way to get a unique file path

--- cmd/ww/file.go
+++ cmd/ww/file.go
@@ -7,6 +7,10 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
 )
 
 const (
@@ -23,24 +27,65 @@ type header struct {
 
 // find a suitable filename to receive a file. if the path already exist, append a suffix or increment the existing suffix
 func getUniquePath(path string) string {
-	newpath := path
-	suffixpattern := regexp.MustCompile(`\.(\d+)$`)
-	// see if the file already exists
-	if _, err := os.Stat(path); err == nil { //nil error means file exists
-		// see if the file alreay has a .1, .2, etc. suffix
-		if matches := suffixpattern.FindAllStringSubmatch(path, 1); len(matches) > 0 {
-			// if so, increment the suffix
-			currentSuffix, _ := strconv.Atoi(matches[0][1])
-			newpath = suffixpattern.ReplaceAllString(path, fmt.Sprintf(".%d", currentSuffix+1))
-		} else {
-			// if not, add .1 to the name
-			newpath += ".1"
-		}
-	} else {
-		return newpath
+
+	if _, err := os.Stat(path); err != nil {
+		return path
 	}
-	// if we're not returning in the above else case, we don't know the new path exists or not. hence, recurse
-	return getUniquePath(newpath)
+
+	// find the last . (dot char)
+	lastDot := strings.LastIndex(path, ".")
+	filenameAndSuffix, extension := path[:lastDot], path[lastDot:]
+	lastUnderscore := strings.LastIndex(filenameAndSuffix, "_")
+	// if there's no underscore found, add it to the filename
+	if lastUnderscore == -1 {
+		return getUniquePath(fmt.Sprintf("%s_%d%s", filenameAndSuffix, 1, extension))
+	}
+	// if there's already an underscore, check to see if it's a number, if it is, increment it, otherwise, add the underscore as if there was none
+	filename, suffix := filenameAndSuffix[:lastUnderscore], filenameAndSuffix[lastUnderscore:]
+	// check if suffix is a number
+	if n, err := strconv.Atoi(suffix[1:]); err == nil {
+		//increment and re-call the function just in case there are multiple files being replaced
+		return getUniquePath(fmt.Sprintf("%s_%d%s", filename, n+1, extension))
+	}
+
+	return getUniquePath(fmt.Sprintf("%s_%d%s", filenameAndSuffix, 1, extension))
+
+}
+
+// Test_getUniquePath is a simple test suite for above getUniquePath function.
+// NOTE: it is very unlikely that you'd have filenames like the ones created below
+// but proceed with caution nonetheless
+func Test_getUniquePath(t *testing.T) {
+	unixNano := time.Now().Format("2006-01-02T15:04:05.999999-07:00")
+	ext := "txt"
+	f1 := fmt.Sprintf("%s.%s", unixNano, ext)
+	_, _ = os.Create(f1)
+	defer os.Remove(f1)
+	f2 := fmt.Sprintf("%s_1.%s", unixNano, ext)
+	_, _ = os.Create(f2)
+	defer os.Remove(f2)
+	f3 := fmt.Sprintf("%s_notnumber.%s", unixNano, ext)
+	_, _ = os.Create(f3)
+	defer os.Remove(f3)
+
+	tests := []struct {
+		name string
+		path string
+		want string
+	}{
+		// test1 should return _2 because _1 already exists
+		{name: "test1", path: f1, want: fmt.Sprintf("%s_2.%s", unixNano, ext)},
+		// test2 should return _2 because it's an increment to _1
+		{name: "test2", path: f2, want: fmt.Sprintf("%s_2.%s", unixNano, ext)},
+		{name: "test3", path: f3, want: fmt.Sprintf("%s_notnumber_1.%s", unixNano, ext)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getUniquePath(tt.path); got != tt.want {
+				t.Errorf("getUniquePath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
 func receive(args ...string) {
